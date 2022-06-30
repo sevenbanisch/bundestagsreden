e31338be5aca6c1d7ff6b648997bd50225ac3ba2
Sven Banisch <sven.banisch@universec ity.de>
2022-06-30 16:47:30 +0200

    add filter_for to BTTools, change folder structure
    All utilities are now in folder utilities

---
+++ utilities/BTTools.py
@@ -0,0 +1,22 @@
+
+
+def filter_for(what, search_terms, speeches):
+    filtered_speeches = []
+    if what == 'text':
+        search_terms_low = []
+        for st in search_terms:
+            search_terms_low.append(st.lower())
+        for speech in speeches:
+            match = [st in speech[what].lower() for st in search_terms_low]
+            #if all(st == True for st in match):
+            if any(st in speech[what] for st in search_terms):
+                #print(match)
+            #if ( search_terms in speech[what] ):
+                filtered_speeches.append(speech)
+    else:
+        for speech in speeches:
+            if ( speech[what] in set(search_terms) ):
+                filtered_speeches.append(speech)
+        
+    filtered_speeches.sort(key = lambda x:x['date'])   
+    return filtered_speeches
