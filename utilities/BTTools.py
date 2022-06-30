f5eb8d5a51111ac13a1fca91d04b4ff667c04039
Sven Banisch <sven.banisch@universec ity.de>
2022-06-30 17:26:17 +0200

    testing of regrouping

--- utilities/BTTools.py
+++ utilities/BTTools.py
@@ -21,6 +21,8 @@ def filter_for(what, search_terms, speeches):
     filtered_speeches.sort(key = lambda x:x['date'])   
     return filtered_speeches
 
+
+
 def groupSpeechesByDiscussionTitle(speeches):
     groupedSpeeches = {}
     for speech in speeches:
