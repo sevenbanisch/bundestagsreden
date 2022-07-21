6107cfd7725923ed1a885060f34ce8fe78bf87a4
Sven Banisch <43904247+sevenbanisch@users.noreply.github.com>
2022-07-21 14:58:29 +0200

    recover changes from 30.06. to parser.py run.py

--- bundestagsreden parser/run.py
+++ bundestagsreden parser/run.py
@@ -3,7 +3,12 @@
 import sys
 from src.parser import parser
 
-if len(sys.argv) != 2:
-    raise ValueError("Enter number of legislative Period as integer. E.g.: 19")
+#if len(sys.argv) != 2:
+#    raise ValueError("Enter number of legislative Period as integer. E.g.: 19")
 
-parser(sys.argv[1])
+
+period = input("Select legislative period (19 or 20): ")
+
+withcomments = input("Shall comments be included? (y or n): ")
+
+parser(period,withcomments)
